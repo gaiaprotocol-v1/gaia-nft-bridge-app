d275bcc8608bc4aa2c971e6fe8137f4c9748c322
dilrong <dilrong@dilrong.com>
2022-08-14 15:55:30 +0900

    styles: img checkbox

--- src/component/NftItem.ts
+++ src/component/NftItem.ts
@@ -1,21 +1,25 @@
-import { DomNode, el } from "skydapp-browser";
+import { DomNode, el, Store } from "skydapp-browser";
 
 export default class NftItem extends DomNode {
+
+    public store = new Store("nft-store");
 
     private checkbox: DomNode<HTMLInputElement>;
 
     constructor(name: string, public id: number) {
         super(".nft-item");
         this.append(
-            el("img", { src: `https://storage.googleapis.com/gaia-protocol/${name === "genesis" ? "kronos" : name}/${id}.png`, alt: `nft${id}` }),
-            el(".checkbox-container",
-                this.checkbox = el("input", { type: "checkbox", id: `nft${id}` }, {
-                    change: () => {
-                        this.fireEvent(this.checkbox.domElement.checked === true ? "selected" : "deselected");
-                    },
-                }),
-                el("label", { for: `nft${id}` }),
-                el("p", `#${id}`),
+            el(".content", { click: () => { this.checkbox.domElement.checked = !this.checkbox.domElement.checked; } },
+                el("img", { src: `https://storage.googleapis.com/gaia-protocol/${name === "genesis" ? "kronos" : name}/${id}.png`, alt: `nft${id}` }),
+                el(".checkbox-container",
+                    this.checkbox = el("input", { type: "checkbox", id: `nft${id}` }, {
+                        change: () => {
+                            this.fireEvent(this.checkbox.domElement.checked === true ? "selected" : "deselected");
+                        },
+                    }),
+                    el("label", { for: `nft${id}` }),
+                    el("p", `#${id}`),
+                ),
             ),
         );
     }
